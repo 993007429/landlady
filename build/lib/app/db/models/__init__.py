008334632e205e78c9e8156d0d9a32060cd72ebd
zhaoyu <zhaoyu@lukou.com>
2021-10-27 16:15:56 +0800

    remove useless files

--- build/lib/app/db/models/__init__.py
+++
@@ -1,9 +0,0 @@
-from app.db.models.project import Project
-from app.db.models.box import Box
-from app.db.models.user import User
-
-
-__all__ = [
-    'Box', 'Project', 'User'
-]
-
--- build/lib/app/db/types.py
+++
@@ -1,5 +0,0 @@
-from typing import TypeVar
-
-from app.db.models import Box, Project, User
-
-M = TypeVar('M', Project, Box, User)
